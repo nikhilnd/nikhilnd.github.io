9d4724f4633966750a8cdf34661438875c311fcc
Nikhil Deorkar <nikhil.deorkar@gmail.com>
2016-11-11 11:47:31 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(function() {
+  $('.github').mouseenter(function() {
+    $('.github').addClass('animated tada');
+  });
+
+  $('.github').mouseleave(function() {
+    $('.github').removeClass('animated tada');
+  });
+
+  $('.email').mouseenter(function() {
+    $('.email').addClass('animated tada');
+  });
+
+  $('.email').mouseleave(function() {
+    $('.email').removeClass('animated tada');
+  });
+});
