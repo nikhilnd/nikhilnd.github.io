325f87827b3dab74b64f3fff38a62a6274d8f9cb
Nikhil Deorkar <nikhil.deorkar@gmail.com>
2016-11-14 17:06:52 -0500

    Fixed text

--- js/app.js
+++ js/app.js
@@ -15,14 +15,14 @@ $(function() {
     $('.email').removeClass('animated tada');
   });
 
-  var movementStrength = 25;
-  var height = movementStrength / $(window).height();
-  var width = movementStrength / $(window).width();
-  $("header").mousemove(function(e){
-    var pageX = e.pageX - ($(window).width() / 2);
-    var pageY = e.pageY - ($(window).height() / 2);
-    var newvalueX = width * pageX * -1 - 25;
-    var newvalueY = height * pageY * -1 - 50;
-    $("header").css("background-position", newvalueX+"px     "+newvalueY+"px");
-  });
+  // var movementStrength = 25;
+  // var height = movementStrength / $(window).height();
+  // var width = movementStrength / $(window).width();
+  // $("header").mousemove(function(e){
+  //   var pageX = e.pageX - ($(window).width() / 2);
+  //   var pageY = e.pageY - ($(window).height() / 2);
+  //   var newvalueX = width * pageX * -1 - 25;
+  //   var newvalueY = height * pageY * -1 - 50;
+  //   $("header").css("background-position", newvalueX+"px     "+newvalueY+"px");
+  // });
 });
