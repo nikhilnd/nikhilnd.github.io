2799f84046ebe0e49978d06639f154afd05af812
Nikhil Deorkar <nikhil.deorkar@gmail.com>
2017-06-11 16:56:21 -0400

    add bg effect

--- js/app.js
+++ js/app.js
@@ -15,14 +15,9 @@ $(function() {
     $('.email').removeClass('animated tada');
   });
 
-  // var movementStrength = 25;
-  // var height = movementStrength / $(window).height();
-  // var width = movementStrength / $(window).width();
-  // $("header").mousemove(function(e){
-  //   var pageX = e.pageX - ($(window).width() / 2);
-  //   var pageY = e.pageY - ($(window).height() / 2);
-  //   var newvalueX = width * pageX * -1 - 25;
-  //   var newvalueY = height * pageY * -1 - 50;
-  //   $("header").css("background-position", newvalueX+"px     "+newvalueY+"px");
-  // });
+  $('header').mousemove(function(e){
+    var x = -(e.pageX + this.offsetLeft) / 25;
+    var y = -(e.pageY + this.offsetTop) / 25;
+    $(this).css('background-position', x + 'px ' + y + 'px');
+  });
 });
